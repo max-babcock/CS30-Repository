b9327a82dece542c044fe57d287d4993e0c11965
max-babcock <7005402@spsd.sk.ca>
2024-11-06 15:05:56 -0600

    11/6/2024

--- JS Demo's/13 Tic-Tac-Toe/sketch.js
+++ JS Demo's/13 Tic-Tac-Toe/sketch.js
@@ -1,15 +1,38 @@
-// Project Title
-// Your Name
-// Date
+// Tic-Tac-Toe
+// Max Babcock
+// 11/6/2024
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
+let tiles = []
+
+let grid = [
+  [0,0,0],
+  [0,0,0],
+  [0,0,0]
+]
+
+let rectWidth, rectHeight;
+const COLUMNS = 3, ROWS = 3;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  stroke(0);
+  rectWidth = width / COLUMNS;
+  rectHeight = height / ROWS;
 }
 
 function draw() {
   background(220);
+  renderBoard();
+  determineActiveSquare();
+}
+
+
+function renderBoard(){
+  
+}
+
+
+function determineActiveSquare(){
+
 }
